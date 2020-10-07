df675f02597f9556a68f4347fa7cc3992f53f6e3
sergey <serega80422@gmail.com>
2020-10-07 13:30:38 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+// // alert("hello");
+
+// // const result = confirm("Тебе есть 18?");
+// // console.log(result);
+
+// // const result = prompt("Сколько вчера выпил?", "");
+// // console.log(result);
+
+// const answers = [];
+
+// // answers[0] = prompt("Как дела?", "");
+// // answers[1] = prompt("Как с учебой?", "");
+// // answers[2] = prompt("Что вечером делаешь?", "");
+
+// console.log(typeof(answers));
+
+
+
+
+const user = 'Sergey';
+
+alert(`Hello, ${user}`);
